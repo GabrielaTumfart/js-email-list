0210afecdc168780335f38d1b52d204b4228a1d7
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-01-22 16:33:28 +0100

    Modificato <p> in <ul> in HTML per mostrare più email

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
+
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
 .then(response => {
-    cosnt email = response.data.response;
+    const email = response.data.response;
 
     //prendo elemento del DOM
     document.getElementById("email").textContent = email;
