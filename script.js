73ff1f84afdd7829a6f6364cf835427b4ec26b93
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-01-22 16:28:25 +0100

    cerco l'errore

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
 .then(response => {
+    cosnt email = response.data.response;
+
+    //prendo elemento del DOM
+    document.getElementById("email").textContent = email;
 
 });
